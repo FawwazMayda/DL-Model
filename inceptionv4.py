1e95128cc39cb676bddf57d6345bfb09e316779e
fawwazmayda <maydafawwaz@gmail.com>
2019-11-18 20:59:24 +0700

    Reduction A

--- inceptionv4.py
+++ inceptionv4.py
@@ -51,6 +51,32 @@ def inception_a(input):
 
     x= keras.layers.concatenate([b0,b1,b2,b3],axis=channel_axis)
     return x
+def reduction_a(input):
+    #Mengingat Konfigurasi Keras bisa channel first atau last
+    if K.image_data_format() == 'channels_first':
+        channel_axis = 1
+    else:
+        channel_axis = -1
+
+    b0 = conv2d_bn(input,384,3,3,strides=(2,2),padding='valid')
+
+    b1 = conv2d_bn(input,192,1,1)
+    b1 = conv2d_bn(b1,224,3,3)
+    b1 = conv2d_bn(b1,256,3,3,strides=(2,2),padding='valid')
+
+    b2 = keras.layers.MaxPooling2D((3,3),strides=(2,2),padding='valid')
+
+    x = keras.layers.concatenate([b0,b1,b2],axis=channel_axis)
+
+def inception_b(input):
+    #Mengingat Konfigurasi Keras bisa channel first atau last
+    if K.image_data_format() == 'channels_first':
+        channel_axis = 1
+    else:
+        channel_axis = -1
+
+
+
 
 
 
