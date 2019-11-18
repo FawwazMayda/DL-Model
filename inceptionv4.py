a1f23e0fd0c646c458c1383ff5baa78644158791
fawwazmayda <maydafawwaz@gmail.com>
2019-11-18 20:00:24 +0700

    Add Weights PATH

---
+++ inceptionv4.py
@@ -0,0 +1,6 @@
+import keras
+from keras import backend as K 
+
+WEIGHTS_PATH = 'https://github.com/kentsommer/keras-inceptionV4/releases/download/2.1/inception-v4_weights_tf_dim_ordering_tf_kernels.h5'
+WEIGHTS_PATH_NO_TOP = 'https://github.com/kentsommer/keras-inceptionV4/releases/download/2.1/inception-v4_weights_tf_dim_ordering_tf_kernels_notop.h5'
+
