7f6b4b2bc2f7f87b9050380b32a00a6e16f143f3
fawwazmayda <maydafawwaz@gmail.com>
2019-11-18 20:01:45 +0700

    Pre Process

--- inceptionv4.py
+++ inceptionv4.py
@@ -1,6 +1,12 @@
+import numpy as np 
 import keras
 from keras import backend as K 
 
 WEIGHTS_PATH = 'https://github.com/kentsommer/keras-inceptionV4/releases/download/2.1/inception-v4_weights_tf_dim_ordering_tf_kernels.h5'
 WEIGHTS_PATH_NO_TOP = 'https://github.com/kentsommer/keras-inceptionV4/releases/download/2.1/inception-v4_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
+def preprocess_input(x):
+    x = np.divide(x,255.0)
+    x = np.subtract(x , 0.5)
+    x = np.multiply(x, 2.0)
+    return x
